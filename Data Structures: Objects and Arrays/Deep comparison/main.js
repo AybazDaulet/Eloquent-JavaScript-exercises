b1a2ee3ba621933c5cd529b7e9d62264f59fcc13
Daulet Aybaz <daulet.cubex@gmail.com>
2024-04-27 14:41:46 -0400

    Deep comparison minor fix

--- Data Structures: Objects and Arrays/Deep comparison/main.js
+++ Data Structures: Objects and Arrays/Deep comparison/main.js
@@ -1,5 +1,5 @@
 const deepEqual = function (value1, value2) {
-    if (value1 == value2) return true;
+    if (value1 === value2) return true;
     if (value1 == null || value2 == null || typeof value1 != "object" || typeof value2 != "object") return false;
     let keys1 = Object.keys(value1), keys2 = Object.keys(value2);
     if (keys1.length != keys2.length) return false;
